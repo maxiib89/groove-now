3339ed58554c4a131e93bdcd401a75bb19fc9de7
maxiib89 <maxii.b89@gmail.com>
2016-06-01 16:29:04 -0300

    Add files via upload

---
+++ js/controllers/MainCtrl.js
@@ -0,0 +1,16 @@
+{
+    "name": "starter-node-angular",
+    "version": "1.0.0",
+    "dependencies": {
+        "bootstrap": "latest",
+        "font-awesome": "latest",
+        "animate.css": "latest",
+        "angular": "latest",
+        "angular-route": "latest"   
+    }
+}
+            // route for the home page
+            .when('/', {
+                templateUrl : 'pages/home.html',
+                controller  : 'mainController'
+            })
